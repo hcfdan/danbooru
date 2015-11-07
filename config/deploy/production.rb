e4edc53455673ec107b7813c6472ad17f5545b43
Iratu <admin@rori-dl.com>
2015-11-07 17:43:21 +0100

    modifications.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :user, "danbooru"
 set :rails_env, "production"
-server "sonohara.donmai.us", :roles => %w(web app db), :primary => true, :user => "danbooru"
+server "localhost", :roles => %w(web app db), :primary => true, :user => "danbooru"
 
